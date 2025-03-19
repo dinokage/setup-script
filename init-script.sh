07d3b3352db7899bf3cafa3f3563e324dc89f1b3
Dinesh Yerra <68950956+dinokage@users.noreply.github.com>
2025-03-19 12:33:17 +0530

    remove cretbot + add node

--- init-script.sh
+++ init-script.sh
@@ -2,9 +2,26 @@
 
 sudo apt update && sudo apt upgrade -y
 
+sudo apt install git wget net-tools -y
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.14.0".
+nvm current # Should print "v22.14.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
 
 # Add Docker's official GPG key:
-sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -19,11 +36,3 @@ sudo apt-get update
 
 # Install docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Certbot setup
-
-sudo snap install certbot --classic
-
-# start certbot process
-
-sudo certbot --nginx
